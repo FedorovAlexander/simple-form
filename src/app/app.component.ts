d045c01bd2eaa1d1b9b519214bc8ba3d9cf3e9a8
Aleks Fedorov <hamster2240@gmail.com>
2025-06-29 11:38:39 +0300

    Chore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FormComponent } from './components/form/form.component';
 
 @Component({
-    selector: 'app-root',
-    imports: [RouterOutlet, FormComponent],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {}
